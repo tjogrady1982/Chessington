62ac39a1e8e874677174a2f4beee3b50191ace26
Tim O'Grady <tim.ogrady@anthonynolan.org>
2018-04-11 15:42:41 +0100

    King moves

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,67 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var pos = board.FindPiece(this);
+            var legalMoves = new List<Square>();
+
+            var placeICanMove = new Square(pos.Row , pos.Col + 1);
+
+            if (placeICanMove != pos & placeICanMove.Row < 8 & placeICanMove.Col < 8)
+            {
+                legalMoves.Add(placeICanMove);
+            }
+
+            placeICanMove = new Square(pos.Row , pos.Col - 1);
+
+            if (placeICanMove != pos & placeICanMove.Row > -1 & placeICanMove.Col > -1)
+            {
+                legalMoves.Add(placeICanMove);
+            }
+
+
+            placeICanMove = new Square(pos.Row + 1, pos.Col);
+
+            if (placeICanMove != pos & placeICanMove.Row < 8 & placeICanMove.Col > -1)
+            {
+                legalMoves.Add(placeICanMove);
+            }
+
+            placeICanMove = new Square(pos.Row - 1, pos.Col);
+
+            if (placeICanMove != pos & placeICanMove.Row > -1 & placeICanMove.Col < 8)
+            {
+                legalMoves.Add(placeICanMove);
+            }
+
+            placeICanMove = new Square(pos.Row+ 1, pos.Col + 1);
+
+            if (placeICanMove != pos & placeICanMove.Row < 8 & placeICanMove.Col < 8)
+            {
+                legalMoves.Add(placeICanMove);
+            }
+
+            placeICanMove = new Square(pos.Row + 1, pos.Col -1);
+
+            if (placeICanMove != pos & placeICanMove.Row < 8 & placeICanMove.Col > -1)
+            {
+                legalMoves.Add(placeICanMove);
+            }
+
+            placeICanMove = new Square(pos.Row - 1, pos.Col + 1);
+
+            if (placeICanMove != pos & placeICanMove.Row > -1 & placeICanMove.Col < 8)
+            {
+                legalMoves.Add(placeICanMove);
+            }
+
+            placeICanMove = new Square(pos.Row - 1, pos.Col - 1);
+
+            if (placeICanMove != pos & placeICanMove.Row > -1 & placeICanMove.Col > -1)
+            {
+                legalMoves.Add(placeICanMove);
+            }
+
+            return legalMoves;
         }
     }
 }
