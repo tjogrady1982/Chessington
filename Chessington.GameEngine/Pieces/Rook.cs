734a272de7bb1acebf2766519e0a4b1b8adcac08
Tim O'Grady <tim.ogrady@anthonynolan.org>
2018-04-11 14:38:44 +0100

    Rookery

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,7 +11,34 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+
+            var pos = board.FindPiece(this);
+            var legalMoves = new List<Square>();
+            
+
+            for (var i = 0; i < 8; i++)
+
+            {
+                
+                var placeICanMove = new Square(pos.Row,  i);
+
+                if (placeICanMove != pos)
+                {
+                    legalMoves.Add(placeICanMove);
+                }
+
+                placeICanMove = new Square(i, pos.Col);
+
+                if (placeICanMove != pos)
+                {
+                    legalMoves.Add(placeICanMove);
+                }
+
+            }
+
+            
+
+            return legalMoves;
         }
     }
 }
