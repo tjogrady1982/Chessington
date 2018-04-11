b683a457ab27d475fd2984f2f8ac133849e67c5f
Tim O'Grady <tim.ogrady@anthonynolan.org>
2018-04-11 12:10:41 +0100

    pawns show available moves

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,31 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            
+           var column = board.FindPiece(this).Col;
+            var row = board.FindPiece(this).Row;
+            if (Player == Player.Black)
+            {
+                return new List<Square>
+                {
+                    Square.At(row + 1, column)
+                };
+            }
+            else if (Player == Player.White)
+
+            {
+                return new List<Square>
+                {
+                    Square.At(row - 1, column)
+                };
+            }
+
+            return new List<Square>
+            {
+                //Square.At(6, 0)
+            };
+
+            //return Enumerable.Empty<Square>();
         }
     }
 }
